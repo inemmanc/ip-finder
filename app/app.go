37c33aff3abb4cfe137ee0c44f7d36aa8a6c1872
Felipe <nemmanc@protonmail.com>
2024-02-14 21:33:18 -0300

    add: name-server-finder

--- app/app.go
+++ app/app.go
@@ -13,18 +13,26 @@ func Generate() *cli.App {
 	app.Name = "IP FINDER APPLICATION"
 	app.Usage = "Find the IP and NAME of a server"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name: "host",
+			// default value
+			Value: "google.com",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Find IP adresses on the internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name: "host",
-					// default value
-					Value: "google.com",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Find IP adresses on the internet",
+			Flags:  flags,
 			Action: ipFinder,
+		},
+		{
+			Name:   "server",
+			Usage:  "Find server names on the internet",
+			Flags:  flags,
+			Action: nameFinder,
 		},
 	}
 
@@ -42,5 +50,18 @@ func ipFinder(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func nameFinder(c *cli.Context) {
+	host := c.String("host")
+
+	servers, err := net.LookupNS(host)
+	if err != nil {
+		fmt.Println("ERROR NAME SERVER")
+	}
+
+	for _, server := range servers {
+		fmt.Println(server)
 	}
 }
